6131070096b8eec8eb0e12cd238ef724ff40228a
Ghifari Laseha <lasehaghifari@gmail.com>
2026-07-04 10:44:21 +0700

    Create seed_users.sql

---
+++ lib/seed_users.sql
@@ -0,0 +1,12 @@
+-- Ganti "Nama Kamu" dengan nama asli kamu sebagai admin
+insert into users (nama, role) values
+  ('Nama Kamu', 'admin'),
+  ('Rangga', 'pelaksana'),
+  ('Anwar', 'pelaksana'),
+  ('Abdullah', 'pelaksana'),
+  ('Nuri', 'pelaksana'),
+  ('Bayu', 'pelaksana'),
+  ('Imam', 'pelaksana'),
+  ('Syahal', 'pelaksana'),
+  ('Ade', 'pelaksana'),
+  ('Reiza', 'pelaksana');
